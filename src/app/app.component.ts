f46e5a2e805e82f1604af8396dacb1e3af09af8e
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 17:25:22 +0100

    Removing firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import firebase from '@firebase/app';
-import '@firebase/auth';
-import '@firebase/firestore';
-import '@firebase/storage';
-import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() {
-    firebase.initializeApp(environment.firebase);
-  }
+  constructor() { }
 }
