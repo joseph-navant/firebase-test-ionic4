03520f2409edb8c757df15a66f45adbb6c32a66a
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 13:07:12 +0100

    Revert "Adding firebase"
    This reverts commit 59a822d2ed46085275f9a5d343d95a4a3a4d3ffc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularFireModule } from '@angular/fire';
-import { environment } from 'src/environments/environment.prod';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
