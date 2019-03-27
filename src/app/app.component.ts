6688f9a439bf590f2e05231700cb0e429262fabe
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 13:14:31 +0100

    Adding firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() { }
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+  }
 }
