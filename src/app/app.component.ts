25ab98c8edf340da0853cb737e7daac79c5d932a
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 17:07:15 +0100

    Adding firebase auth, firestore and storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase/app';
-
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/firestore';
+import '@firebase/storage';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
